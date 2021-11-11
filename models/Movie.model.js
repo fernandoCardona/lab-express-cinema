2c483892d9afc468b8e602923e1d478935fc8024
Fernando Cardona <fernando.cardona@the-lion.com>
2021-11-11 23:03:14 +0100

    no m conecta...

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -1,18 +1,12 @@
 const mongoose = require('mongoose')
 
-const movieSchema = new mongoose.Schema(
-  {
-    title: {type: String,required: true,unique: true,},
-    director: {String},
-    stars: [String],
-    image: {String},
-    description: {String},
-    showtimes: [String],
-  },
-  {
-    timestamps: true,
-  }
-)
+const movieSchema = new mongoose.Schema({
+    title: { type: String, required: true, unique:true },
+    director: { type: String }, 
+    stars: [ String ], 
+    image: { type: String},
+    showtimes: [ String ],
+})
 
 const Movie = mongoose.model('Movie', movieSchema)
 
